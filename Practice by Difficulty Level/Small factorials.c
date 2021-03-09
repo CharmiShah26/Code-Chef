9dffbfa9240f52e8c2d046bedbe40ce2a07dbecc
Charmi Shah <71177651+CharmiShah26@users.noreply.github.com>
2021-03-09 19:49:21 +0530

    Add files via upload

---
+++ Practice by Difficulty Level/Small factorials.c
@@ -0,0 +1,62 @@
+/*#include<stdio.h>
+int fact(int y);
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    while(n!=0)
+    {
+        int x,value;
+        scanf("%d",&x);
+        value=fact(x);
+        printf("%d\n",value);
+        value=0;
+        n--;
+    }
+}
+int fact(int y)
+{
+    if(y==0 || y==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return y*fact(y-1);
+    }
+}*/
+#include<stdio.h>
+
+int main()
+{
+    int t,n,a[200],k,l,m;
+    scanf("%d",&t);
+    while(t--)
+    {
+	    scanf("%d",&n);
+        m=1;
+        a[0]=1;
+        for(int j=2;j<=n;j++)
+        {
+            l=0;
+            for(k=0;k<m;k++)
+            {
+                a[k]=a[k]*j+l;//a[0]=2, a[0]=6, a[0]=24 , a[0]=20   ,a[1]=12
+                l=a[k]/10;//l=0,//l=2,//l=2,//l=1
+                a[k]=a[k]%10;//a[0]=6;//a[0]=4,k=1,//a[0]=0//a[1]=2,k=2
+            }
+            while(l)
+            {
+	            a[k]=l%10;//a[1]=1
+                k++;//k=2;
+                m++;//m=2
+                l=l/10;//l=0
+            }
+        }
+        for(int i=m-1;i>=0;i--)
+            printf("%d",a[i]);
+
+        printf("\n");
+    }
+    return 0;
+}
