e7a083a40ace3f6fff72a290ccdf6903b90c4d42
Charmi Shah <71177651+CharmiShah26@users.noreply.github.com>
2021-05-30 19:05:15 +0530

    Add files via upload

---
+++ QUIZPLAG.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 30 18:20:12 2021
+
+@author: charm
+"""
+
+def CountFrequency(my_list):
+    freq = {}
+    for item in my_list:
+            if (item in freq):
+                    freq[item] += 1
+            else:
+                    freq[item] = 1
+    return freq
+
+
+for _ in range(int(input())):
+    n,m,k=map(int,input().split())
+    arr=list(map(int,input().split()))
+    freq=CountFrequency(arr)
+    ans=[]
+    count=0
+    for key,values in freq.items():
+        if(key<=n and values>1):
+            ans.append(key)
+            count=count+1
+    ans.sort()
+    ans.insert(0,count)
+    print(*ans)
